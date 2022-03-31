269c5f1c1a7f340ba7685e6773458205acebe131
Sarvang Konnur <36808350+darkvo1d@users.noreply.github.com>
2022-03-31 04:27:57 -0400

    Initial Commit
    App works...
    TODO: improve predictions... most probable reason: image quality downscales

---
+++ FlaskApp.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 05 11:45:03 2022
+
+@author: noopa
+"""
+import numpy as np
+import cv2
+from keras.models import load_model
+from flask import Flask, request, render_template
+from utils import preprocess, value_invert, data_uri_to_cv2_img
+
+app = Flask(__name__)
+model = load_model("classifier.h5")
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    print("BBBB")
+    # Read the image data from a base64 data URL
+    imgstring = request.form.get('data')
+
+    # Convert to OpenCV image
+    img = preprocess(data_uri_to_cv2_img(imgstring))
+    cv2.imwrite('static/user_drawn/temp.png', img)
+
+    data = value_invert(img / 255).reshape((1, 28, 28, 1))
+
+    prediction = model.predict(data)
+    classes_x = np.argmax(prediction, axis=1)
+    predicted_class = classes_x
+
+    return f"The digit drawn looks like {predicted_class}"
+
+    return render_template('index.html', prediction_text='The digit drawn looks like {}'.format(predicted_class))
+
+
+if __name__ == '__main__':
+    app.run()
